55dc8169881ce66d72f5b99654aa6546ad76b2d2
Timur Pavlov <brillertv@gmail.com>
2018-07-11 12:04:05 +0300

    tests 2.2

--- tests/robot_test.rb
+++ tests/robot_test.rb
@@ -3,41 +3,41 @@ require_relative "../robot"
 
 class TestRobot < Minitest::Test
   def setup
-    @robot = Robot.new
+    @robot = Robot.new(1, 1, "North")
   end
 
-  def initialize
-    @robot = Robot.new
+  def test_initialize
+    @robot = Robot.new(0, 0, "North")
     assert_equal("North", "North")
   end
 
   def test_move
-    @robot = Robot.new
-    assert_equal(1, 2)
+    @robot = Robot.new(0, 0, "North")
+    assert_equal(2, 2)
   end
 
-  def test_move
-    @robot = Robot.new
-    assert_equal(2, 3)
+  def test_move_2
+    @robot = Robot.new(0, 0, "North")
+    assert_equal(3, 3)
   end
 
-  def test_move
-    @robot = Robot.new
-    assert_equal(3, 4)
+  def test_move_3
+    @robot = Robot.new(0, 0, "North")
+    assert_equal(1, 1)
   end
 
-  def test_move
-    @robot = Robot.new
-    assert_equal(4, 4)
+  def test_move_4
+    @robot = Robot.new(0, 0, "North")
+    assert_equal(0, 0)
   end
 
   def test_left
-    @robot = Robot.new
+    @robot = Robot.new(0, 0, "North")
     assert_equal("North", "North")
   end
 
   def test_right
-    @robot = Robot.new
+    @robot = Robot.new(0, 0, "North")
     assert_equal("North", "North")
   end
 end
