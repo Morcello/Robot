a3a51a86674998bfb29e7cbeedf903a309ce7ba4
Timur Pavlov <brillertv@gmail.com>
2018-07-13 20:29:16 +0300

    tests 1.3

--- tests/robot_test.rb
+++ tests/robot_test.rb
@@ -2,42 +2,30 @@ require "minitest/autorun"
 require_relative "../robot"
 
 class TestRobot < Minitest::Test
-  def setup
-    @robot = Robot.new(1, 1, "North")
-  end
-
   def test_initialize
-    @robot = Robot.new(0, 0, "North")
-    assert_equal("North", "North")
+    @robot = Robot.new(2, 4, "North")
+    assert_equal([2, 4, "North"], @robot.report)
+    #puts "#{@robot.report}"
   end
 
   def test_move
-    @robot = Robot.new(0, 0, "North")
-    assert_equal(2, 2)
-  end
-
-  def test_move_2
-    @robot = Robot.new(0, 0, "North")
-    assert_equal(3, 3)
-  end
-
-  def test_move_3
-    @robot = Robot.new(0, 0, "North")
-    assert_equal(1, 1)
-  end
-
-  def test_move_4
-    @robot = Robot.new(0, 0, "North")
-    assert_equal(0, 0)
+    @robot = Robot.new(3, 3, "West")
+    assert_equal([3, 3, "West"], @robot.report)
+    assert_equal([3, 3, "West"], @robot.report)
+    assert_equal([3, 3, "West"], @robot.report)
+    assert_equal([3, 3, "West"], @robot.report)
+    #puts "#{@robot.report}"
   end
 
   def test_left
-    @robot = Robot.new(0, 0, "North")
-    assert_equal("North", "North")
+    @robot = Robot.new(2, 4, "West")
+    assert_equal([2, 4, "West"], @robot.report)
+    #puts "#{@robot.report}"
   end
 
   def test_right
-    @robot = Robot.new(0, 0, "North")
-    assert_equal("North", "North")
+    @robot = Robot.new(1, 1, "North")
+    assert_equal([1, 1, "North"], @robot.report)
+    #puts "#{@robot.report}"
   end
 end
